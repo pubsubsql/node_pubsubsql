927e40ab67708956fb67c033ada7b337d4455c56
Marin Bek <marin.bek@gmail.com>
2015-12-13 09:05:02 +0100

    basic functionality

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,35 @@
 var pss = require('../'),
-    client = pss.createClient();
+    client = pss.createClient(),
+    async = require('async');
 
 client.on('error', function(err){
     console.error('Got error:', err.message);
 });
 
 client.on('end', function(){
-    console.error('CONNECTION ENDED');
+    console.log('CONNECTION ENDED');
 });
+
+client.on('ready', function() {
+    console.log('CONNECTION READY');
+
+    async.waterfall([
+        function(cb) {
+            client.query('key Stocks Ticker', function(response, err) {
+                console.log("GOT RESPONSE ON COMMAND:", response, err);
+                cb();
+            });
+        }, function(cb) {
+            client.query('tag Stocks MarketCap', function(response, err) {
+                console.log("GOT RESPONSE ON COMMAND:", response, err);
+                cb();
+            });
+        }, function(cb) {
+            client.query("subscribe * from Stocks where MarketCap = 'MEGA CAP'", function(response, err) {
+                console.log("GOT RESPONSE ON COMMAND:", response, err);
+                cb();
+            });
+        }
+    ]);
+})
 
